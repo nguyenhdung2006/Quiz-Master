3205a2418c009036fdf046af668c37ff48e5a318
HuyDung <nguyenhdung2006@gmail.com>
2026-06-18 21:31:43 +0700

    Initial Spring Boot project

---
+++ backend/src/main/java/com/quizmaster/QuizmasterApplication.java
@@ -0,0 +1,13 @@
+package com.quizmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizmasterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/quizmaster/QuizmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
